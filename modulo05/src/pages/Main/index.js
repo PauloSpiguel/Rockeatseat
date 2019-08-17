0cc7dd16ae6ba0ea27173501858df0b6d5e578e5
Paulo Spiguel <paulospil@hotmail.com>
2019-08-17 16:25:50 +0100

    Modulo05 Config-Padrão

---
+++ modulo05/src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Title } from './styles';
+
+function Main() {
+  return (
+    <Title>
+      Main
+      <p>Detalhes do Main</p>
+    </Title>
+  );
+}
+
+export default Main;
---
+++ modulo05/src/pages/Main/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  background: #fff000;
+  font-size: 48px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #7159c1;
+  text-align: center;
+
+  p {
+    font-size: 12px;
+    color: #333;
+    padding: 0;
+    margin-top: 0px;
+  }
+`;
